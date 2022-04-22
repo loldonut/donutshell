a53d15b0a4950797e460d38ef82172cca1141fa2
loldonut <loldonutph@gmail.com>
2022-04-22 18:49:03 +0800

    properly handle exceptions in cmd `ls`

--- donutshell/builtin_modules/ls.py
+++ donutshell/builtin_modules/ls.py
@@ -6,19 +6,14 @@ def ls(text: str) -> None:
     if len(split_text) > 0: 
         try:
             path = split_text[1]
-        except:
-            pass
+        except IndexError: pass
 
     try:
         files = os.listdir(path)
 
         for file in files:
             print(file)
-    except (FileNotFoundError, PermissionError) as e:
-        error_str = str(e)
-
-        # Permission Denied Error
-        if error_str.startswith('[Errno 13]'):
-            print(f'Cannot Access \'{path}\' (Permission Denied)')
-        elif error_str.startswith('[Errno 2]'):
-            print('No such directory as ' + path)
+    except PermissionError:
+        print(f'Cannot Access \'{path}\' (Permission Denied)')
+    except FileNotFoundError:
+        print('No such directory as ' + path)
